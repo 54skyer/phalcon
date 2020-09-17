76b909b72f4c3c5c195aed3a63d1d51b6b00ea37
skyer.zht <405661806@qq.com>
2020-09-17 20:17:27 +0800

    1、安装phalcon-devtools 2、使用 phalcon-devtools 创建一个多模块项目

---
+++ modules/app/modules/frontend/controllers/IndexController.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+
+namespace Modules\Modules\Frontend\Controllers;
+
+class IndexController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
